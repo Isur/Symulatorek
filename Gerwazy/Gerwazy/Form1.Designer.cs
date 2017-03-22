00ea798f0c8c91e0118f6c38eb76c546e8b79298
Artur <arturbed96@gmail.com>
2017-03-22 17:28:57 +0100

    UPDATE

--- Gerwazy/Gerwazy/Form1.Designer.cs
+++ Gerwazy/Gerwazy/Form1.Designer.cs
@@ -33,6 +33,14 @@
             this.label_decode = new System.Windows.Forms.Label();
             this.checkBox_random = new System.Windows.Forms.CheckBox();
             this.checkBox_periodicity = new System.Windows.Forms.CheckBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.groupBox_cards = new System.Windows.Forms.GroupBox();
+            this.numericUpDown_quantity = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_keyLength = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.groupBox_cards.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_quantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_keyLength)).BeginInit();
             this.SuspendLayout();
             // 
             // button_start
@@ -59,15 +67,44 @@
             this.checkBox_periodicity.Name = "checkBox_periodicity";
             this.checkBox_periodicity.UseVisualStyleBackColor = true;
             // 
+            // numericUpDown
+            // 
+            resources.ApplyResources(this.numericUpDown, "numericUpDown");
+            this.numericUpDown.Name = "numericUpDown";
+            // 
+            // groupBox_cards
+            // 
+            this.groupBox_cards.Controls.Add(this.numericUpDown_keyLength);
+            this.groupBox_cards.Controls.Add(this.numericUpDown_quantity);
+            resources.ApplyResources(this.groupBox_cards, "groupBox_cards");
+            this.groupBox_cards.Name = "groupBox_cards";
+            this.groupBox_cards.TabStop = false;
+            // 
+            // numericUpDown_quantity
+            // 
+            resources.ApplyResources(this.numericUpDown_quantity, "numericUpDown_quantity");
+            this.numericUpDown_quantity.Name = "numericUpDown_quantity";
+            // 
+            // numericUpDown_keyLength
+            // 
+            resources.ApplyResources(this.numericUpDown_keyLength, "numericUpDown_keyLength");
+            this.numericUpDown_keyLength.Name = "numericUpDown_keyLength";
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox_cards);
+            this.Controls.Add(this.numericUpDown);
             this.Controls.Add(this.checkBox_periodicity);
             this.Controls.Add(this.checkBox_random);
             this.Controls.Add(this.label_decode);
             this.Controls.Add(this.button_start);
             this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.groupBox_cards.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_quantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_keyLength)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +116,10 @@
         private System.Windows.Forms.Label label_decode;
         private System.Windows.Forms.CheckBox checkBox_random;
         private System.Windows.Forms.CheckBox checkBox_periodicity;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.GroupBox groupBox_cards;
+        private System.Windows.Forms.NumericUpDown numericUpDown_keyLength;
+        private System.Windows.Forms.NumericUpDown numericUpDown_quantity;
     }
 }
 
