e0855e5a5e66c64f1c85d1314930619a58be7177
Artur <arturbed96@gmail.com>
2017-04-11 20:32:47 +0200

    11.04.2017:20:32->ProccessNameChange

--- Gerwazy/TestPrzedSARG04/Properties/AssemblyInfo.cs
+++ Gerwazy/TestPrzedSARG04/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestPrzedSARG04")]
+[assembly: AssemblyTitle("Klucznik Gerwazy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestPrzedSARG04")]
+[assembly: AssemblyProduct("Klucznik Gerwazy")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
